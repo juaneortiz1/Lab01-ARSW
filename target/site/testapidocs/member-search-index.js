4a76b626fd6dc8461d1a336db958a8e498e52261
Juanes <juaneortiz1@gmail.com>
2024-06-11 13:01:34 -0500

    added git ignore

--- target/site/testapidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"edu.escuelaing.arsw.ASE.app","c":"AppTest","l":"AppTest(String)","u":"%3Cinit%3E(java.lang.String)"},{"p":"edu.escuelaing.arsw.ASE.app","c":"AppTest","l":"suite()"},{"p":"edu.escuelaing.arsw.ASE.app","c":"AppTest","l":"testApp()"}];updateSearchResults();
